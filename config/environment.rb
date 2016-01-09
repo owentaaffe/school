ef6c8cdccb47c4dcb8cad02c51c5839e6aa653f5
Owen Taaffe <owentaaffe@gmail.com>
2016-01-09 20:02:00 +0000

    production ready?

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-
+provider :google_auth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
 # Initialize the Rails application.
 Rails.application.initialize!
